a2838af3eaa9fadf6e768b3a7bfa1bef230f788d
Jono <jono.vu@gmail.com>
2019-08-03 01:51:33 +1000

    second changes

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
-import styles from './404.module.css';
+import styles from './404.module.scss';
 
 export default () => (
-    <div>
+    <div className={styles.content}>
         <h1 className={styles.header}>404 Page not found.</h1>
         <Link to="/">Home</Link> | <Link to="/about">About</Link>
         <p className={styles.errorMessage}>
